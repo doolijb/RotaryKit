048c4610e33c98b26be09079febaf1facbf6135c
Jody Doolittle <jody@chariotdev.net>
2025-02-25 15:09:21 -0800

    WIP auth fixes

--- app/src/lib/server/auth/cookies/setUserTokenCookie/index.ts
+++ app/src/lib/server/auth/cookies/setUserTokenCookie/index.ts
@@ -1,10 +1,15 @@
 import type { RequestEvent } from "@sveltejs/kit"
 
+// Convert hours:string to number and calculate the max age for the cookie
+const maxHours = process.env.USER_TOKEN_EXPIRATION_HOURS
+const maxAge = 60 * 60 * Number(maxHours)
+
 export function setUserTokenCookie({ event, token }: { event: RequestEvent; token: string }) {
 	event.cookies.set("userToken", token, {
 		path: "/",
 		httpOnly: true,
 		secure: true,
-		sameSite: "strict"
+		sameSite: "strict",
+		maxAge
 	})
 }
